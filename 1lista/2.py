941c1f6b6ec8cac544325854deaa0598c332cb4d
Davi Antonaji <davi@antonaji.com.br>
2023-07-01 17:16:28 -0300

    Alteração teste

--- 1lista/2.py
+++ 1lista/2.py
@@ -3,4 +3,4 @@ n2 = float(input("Escreva o 2° numero: "))
 n3 = float(input("Escreva o 3° numero: "))
 n4 = float(input("Escreva o 4° numero: "))
 resul = n1 + n2 + n3 + n4
-print(f"O resultado da soma é {resul:.2f}")
+print(f"O resultado da soma é {resul:.2f} ........")
